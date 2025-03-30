29c9735133bb31aa00154ed4e49554f2c9279276
Jessica Dupont <jessica.dupont1990@gmail.com>
2025-03-30 14:14:29 +0200

    base
    structure de base remplie

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Academy',
+    'version': '1.0',
+    'summary': 'Module de gestion scolaire',
+    'description': 'Un module Odoo pour gérer une école.',
+    'author': 'Jessica Dupont, Kevin Host, Ness Gira',
+    'license': 'LGPL-3',
+    'category': 'Sales',
+    'depends': ['base'],  # Dépendance minimale pour fonctionner avec Odoo
+    'data': [
+        #security
+        'security/academy.security.xml',
+        'security/ir.model.access.csv',
+        #data
+        #wizard
+        #views & report
+        'views/academy.menus.xml',#last in list
+    ],
+    'application': True
+}
